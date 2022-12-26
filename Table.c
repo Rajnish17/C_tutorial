d175d8522e0c77de7aacd1b05a037f2c143b7fe7
Rajnish <rajnisharyan12@gmail.com>
2022-12-26 21:43:23 +0530

    Table question

---
+++ Table.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+    printf(" Enter Number for find Table: ");
+    scanf("%d", &num);
+    for(int i= 1; i<=10;i++){
+        printf("%d \n", i*num);
+    }
+    return 0;
+}
--- question.c
+++ question.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int num;
     printf("Enter thu number to find the sum: ");
-    scanf("%d",&num);
-     int sum= 0;
-        
-        
-    for ( int i = 1; i<=num; i++)
+    scanf("%d", &num);
+    int sum = 0;
+
+    for (int i = 1; i <= num; i++)
     {
-     sum =sum+i;  
+        sum = sum + i;
     }
-    printf("%d",sum);
+    printf("%d", sum);
     return 0;
 }
